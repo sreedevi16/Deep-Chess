79deab9ef59f5ed1bb4a63e9446cc3c937098486
Sreedevi S <sridevisunil16@gmail.com>
2026-06-25 00:15:52 +0530

    Update solver_m8n2.cpp

--- week3/solver_m8n2.cpp
+++ week3/solver_m8n2.cpp
@@ -10,60 +10,54 @@ using namespace std;
 using namespace chess;
 using json = nlohmann::json;
 
-string findFirstMove(const string& fen) {
-    Board board(fen);
-    Movelist moves;
-    movegen::legalmoves(moves, board);
+string FirstMove(const string& fen) {
+Board board(fen);
+Movelist moves;
+movegen::legalmoves(moves, board);
+for (const Move& move1 : moves) {
+board.makeMove(move1);
 
-    for (const Move& move1 : moves) {
-        board.makeMove(move1);
+if (board.isCheckmate()) {
+board.unmakeMove(move1);
+return uci::moveToUci(move1);
+}
+if (board.isGameOver().second != GameResult::NONE) {
+board.unmakeMove(move1);
+continue;
+}
 
-        if (board.isCheckmate()) {
-            board.unmakeMove(move1);
-            return uci::moveToUci(move1);
-        }
+bool bool1 = true;
+Movelist blackMove;
+movegen::legalmoves(blackMove, board);
 
-        if (board.isGameOver().second != GameResult::NONE) {
-            board.unmakeMove(move1);
-            continue;
-        }
+for (const Move& move2 : blackMove) {
+board.makeMove(move2);
+bool bool2 = false;
+Movelist whiteMove2;
+movegen::legalmoves(whiteMove2, board);
 
-        bool allRepliesMated = true;
-        Movelist blackMoves;
-        movegen::legalmoves(blackMove, board);
+for (const Move& move3 : whiteMove2) {
+board.makeMove(move3);
+if (board.isCheckmate()) {
+bool2 = true;
+board.unmakeMove(move3);
+break;
+}
+board.unmakeMove(move3);
+}
 
-        for (const Move& move2 : blackMove) {
-            board.makeMove(move2);
+board.unmakeMove(move2);
 
-            bool foundMate = false;
-            Movelist whiteMoves2;
-            movegen::legalmoves(whiteMove2, board);
-
-            for (const Move& move3 : whiteMove2) {
-                board.makeMove(move3);
-                if (board.isCheckmate()) {
-                    foundMate = true;
-                    board.unmakeMove(move3);
-                    break;
-                }
-                board.unmakeMove(move3);
-            }
-
-            board.unmakeMove(move2);
-
-            if (!foundMate) {
-                allRepliesMated = false;
-                break;
-            }
-        }
-
-        board.unmakeMove(move1);
-
-        if (allRepliesMated) {
-            return uci::moveToUci(move1);
-        }
-    }
-
-    return "none";
+if (!bool2) {
+bool1 = false;
+break;
+}
+}
+board.unmakeMove(move1);
+if (bool1) {
+return uci::moveToUci(move1);
+}
+}
+return "none";
 }
 
